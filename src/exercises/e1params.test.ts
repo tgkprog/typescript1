2634556c5319af37eb94d7f424bcf2841cdf34a5
Tushar Kapila <tgkprog@gmail.com>
2023-05-07 07:09:07 +0530

    vitest

---
+++ src/exercises/e1params.test.ts
@@ -0,0 +1,28 @@
+/* export const addTwoNumbers = (params) => {
+    return params.first + params.second;
+  };
+*/
+import { it, expect } from "vitest";
+export const addTwoNumbers = (params: { first: number; second: number }) => {
+  return params.first + params.second;
+};
+
+let ja: { hi: string; bye: number };
+ja = { hi: "za", bye: 4 };
+console.log(ja);
+
+interface Greet {
+  hi: string;
+  bye: number;
+}
+
+it("Add to nos", () => {
+  expect(addTwoNumbers({ first: 4, second: 5 })).toEqual(9);
+
+  expect(addTwoNumbers({ first: 4, second: -5 })).toEqual(-1);
+
+  expect(addTwoNumbers({ first: 8, second: -5 })).toEqual(5); //wrong to check
+});
+//let ja2: object extends Greet;
+//ja2 = { j: {}, hi: "za", bye: 4 };
+//console.log(ja2);
